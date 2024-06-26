9471a3ea4fb1eabb83e6d642805678f44bd5f11a
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-26 10:16:23 -0300

    :sparkles: create product config using fluent api

---
+++ CleanArch/src/CleanArch.Data/EntitiesConfiguration/ProductConfig.cs
@@ -0,0 +1,18 @@
+using CleanArch.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArch.Data.EntitiesConfiguration;
+
+public class ProductConfig : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(product => product.Id);
+        
+        builder.HasOne(product => product.Category)
+            .WithMany(category => category.Products)
+            .HasForeignKey(product => product.CategoryId)
+            .IsRequired();
+    }
+}
