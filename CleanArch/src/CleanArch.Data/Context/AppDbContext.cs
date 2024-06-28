874899a6ed3d42bee12da18550271d8199e64825
Matheus Batista <mbatista.sarti@gmail.com>
2024-06-27 22:17:56 -0300

    :sparkles: insert tables relation

--- CleanArch/src/CleanArch.Data/Context/AppDbContext.cs
+++ CleanArch/src/CleanArch.Data/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
 using CleanArch.Data.EntitiesConfiguration;
+using CleanArch.Data.Utilities;
 using CleanArch.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,14 +10,17 @@ public class AppDbContext : DbContext
     public AppDbContext(DbContextOptions<AppDbContext> context) : base(context)
     { }
     
-    public DbSet<Category> Categories { get; set; }
-    public DbSet<Product> Products { get; set; }
+    public DbSet<Category> Category { get; set; }
+    public DbSet<Product> Product { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        modelBuilder.Entity<Category>().ToTable("category");
+        modelBuilder.Entity<Category>().RenameColumnsNameToLower();
+        modelBuilder.Entity<Product>().ToTable("product");
+        modelBuilder.Entity<Product>().HasOne(product => product.Category).WithMany(category => category.Products).HasForeignKey()
+            .IsRequired();
+        modelBuilder.Entity<Product>().RenameColumnsNameToLower();
         base.OnModelCreating(modelBuilder);
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
-        // modelBuilder.ApplyConfiguration(new CategoryConfig());
-        // modelBuilder.ApplyConfiguration(new ProductConfig());
     }
 }
